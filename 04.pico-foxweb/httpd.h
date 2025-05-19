c52c8171adbb0bda4d5a7fe06f45f00e5914cf28
denis <denis@PC_DEN4IK>
2025-05-19 20:09:38 +0300

    Task 1

--- 04.pico-foxweb/httpd.h
+++ 04.pico-foxweb/httpd.h
@@ -31,10 +31,25 @@ void route();
 // Response
 #define RESPONSE_PROTOCOL "HTTP/1.1"
 
-#define HTTP_200 printf("%s 200 OK\n\n", RESPONSE_PROTOCOL)
-#define HTTP_201 printf("%s 201 Created\n\n", RESPONSE_PROTOCOL)
-#define HTTP_404 printf("%s 404 Not found\n\n", RESPONSE_PROTOCOL)
-#define HTTP_500 printf("%s 500 Internal Server Error\n\n", RESPONSE_PROTOCOL)
+#define HTTP_200 \
+  printf("%s 200 OK\r\n" \
+         "Content-Type: text/html\r\n" \
+         "Connection: close\r\n\r\n", RESPONSE_PROTOCOL)
+
+#define HTTP_201 \
+  printf("%s 201 Created\r\n" \
+         "Content-Type: text/html\r\n" \
+         "Connection: close\r\n\r\n", RESPONSE_PROTOCOL)
+
+#define HTTP_404 \
+  printf("%s 404 Not Found\r\n" \
+         "Content-Type: text/html\r\n" \
+         "Connection: close\r\n\r\n", RESPONSE_PROTOCOL)
+
+#define HTTP_500 \
+  printf("%s 500 Internal Server Error\r\n" \
+         "Content-Type: text/html\r\n" \
+         "Connection: close\r\n\r\n", RESPONSE_PROTOCOL)
 
 // some interesting macro for `route()`
 #define ROUTE_START() if (0) {
